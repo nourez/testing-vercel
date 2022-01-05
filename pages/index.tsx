21e787f256191119acab5824931921aa443b9cd3
nourez <nourez@nourez.dev>
2022-01-04 19:22:21 -0500

    added random id generation via api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next'
+import useSWR from 'swr'
+import axios from 'axios'
+
+const API_URL = 'https://random-data-api.com/api/id_number/random_id_number'
+const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 const Home: NextPage = () => {
-
-  return <h1>Hello There!</h1>
+  const { data, error } = useSWR(API_URL, fetcher)
+  
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return <h1>Hello There User #{data.id}</h1>
 
 }
 
